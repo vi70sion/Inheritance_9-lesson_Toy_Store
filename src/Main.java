9992941b40ff86377e20b6ee174ab070afaffde3
Evaldas Lupeikis <levaldas@hotmail.com>
2024-04-20 17:56:20 +0300

    Added chopping bag

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -14,16 +13,13 @@ public class Main {
 //        Galų gale, parašykite klasę Parduotuvė, kurioje yra sąrašas su turimais žaislais.
 //        Sukurkite metodus, kurie leistų pridėti naujus žaislus į sąrašą, išimti žaislus iš sąrašo ir apskaičiuoti
 //        bendrą žaislų kainą už nurodytą kiekį.
-//        + Sukurkite valdymą krepšelio (žaislų sąrašo) kad pagal pavadinimą galima įdėti
-//        prekes į krepšelį. Prekių krepšelio valdymas turi turėti pasirinkimus - įdėti į krepšelį
-//        ir skaičiuoti krepšelio prekių sumą. (Prekės dedamos į krepšelį ir jo meniu valdomas įvestimi :) )
 
-        Scanner scaner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        Toy toy1 = new Doll("Barbie clone", "made in china", 3, 8.50, DollHairstyle.TWOBRAIDS);
-        Toy toy2 = new Doll("Barbie original", "Mattel", 3, 29.90, DollHairstyle.WATTERFALL);
+        Toy toy1 = new Doll("Barbie_clone", "made in china", 3, 8.50, DollHairstyle.TWOBRAIDS);
+        Toy toy2 = new Doll("Barbie_original", "Mattel", 3, 29.90, DollHairstyle.WATTERFALL);
         Toy toy3 = new Constructor("Battleship", "PlayWrite", 8, 21.00, 216);
-        Toy toy4 = new Constructor("Police car", "Revel", 10, 19.00, 40);
+        Toy toy4 = new Constructor("Police_car", "Revel", 10, 19.00, 40);
 
         ToyStore store = new ToyStore();
         store.addToList(toy1);
@@ -31,30 +27,59 @@ public class Main {
         store.addToList(toy3);
         store.addToList(toy4);
 
-        System.out.println("List of toys:");
+        System.out.println("Žaislų sąrašas:");
         for(Toy item : store.getToyList()){
             System.out.println(item);
         }
         System.out.println();
 
-        System.out.println("Which toy you want to remove (name):");
-        String toyName = scaner.nextLine();
+        System.out.println("Kurį žaislą norite pašalinti (pavadinimas):");
+        String toyName = scanner.nextLine();
         boolean foundToyByName = false;
         for(Toy item : store.getToyList()){
             if (item.getToyName().equals(toyName)){
-                store.getToyList().remove(item);
+                //store.getToyList().remove(item);
+                store.removeFromList(item);
                 foundToyByName = true;
+                break;
             }
         }
-        if(!foundToyByName) System.out.println("Did not find this toy");
-        System.out.println("List of toys:");
-        for(Toy item : store.getToyList()){
-            System.out.println(item);
-        }
+        if(!foundToyByName) System.out.println("Neradome tokio žaislo.");
+        store.printToyList();
         System.out.println();
 
+//        + Sukurkite valdymą krepšelio (žaislų sąrašo) kad pagal pavadinimą galima įdėti
+//        prekes į krepšelį. Prekių krepšelio valdymas turi turėti pasirinkimus - įdėti į krepšelį
+//        ir skaičiuoti krepšelio prekių sumą. (Prekės dedamos į krepšelį ir jo meniu valdomas įvestimi :) )
 
+        String choice = "";
+        do{
+            System.out.println("(1)- prekių sąrašas, (2)- pridėti prekę į krepšelį, (3)- suskaičiuoti krepšelio prekių sumą, (0)- pabaiga");
+            choice = scanner.nextLine();
+            switch (choice){
+                case "1":
+                    store.printToyList();
+                    break;
+                case "2":
+                    int quantity = 0;
+                    System.out.println("Įveskite prekės pavadinimą: ");
+                    toyName = scanner.nextLine();
+                    if(store.findToy(toyName) != null){
+                        System.out.println("Įveskite kiekį: ");
+                        quantity = Integer.parseInt(scanner.nextLine());
+                        store.addToyToShoppingBag(store.findToy(toyName), quantity);
+                        break;
+                    }
+                    System.out.println("Prekė nerasta, pakartokite.");
+                    break;
+                case "3":
+                    store.calculateBagAmount();
+                    break;
+                case "0":
+                    break;
 
+            }
+        } while(!choice.equals("0"));
 
     }
 }
--- src/ToyStore.java
+++ src/ToyStore.java
@@ -1,12 +1,42 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class ToyStore {
     private List<Toy> toyList = new ArrayList<>();
+    private Map<Toy, Integer> shoppingBag = new HashMap();
 
     public List<Toy> getToyList() {return toyList;}
     public void setToyList(List<Toy> toyList) {this.toyList = toyList;}
 
     public void addToList(Toy toy){ this.toyList.add(toy); }
     public void removeFromList(Toy toy){ this.toyList.remove(toy); }
+
+    public void addToyToShoppingBag(Toy toy, int quantity){
+        this.shoppingBag.put(toy, quantity);
+    }
+    public Map<Toy, Integer> getShoppingBag() { return shoppingBag; }
+
+    public void printToyList(){
+        System.out.println("List of toys:");
+        for(Toy item : toyList){
+            System.out.println(item);
+        }
+    }
+    public Toy findToy(String toyName){
+        for(Toy item : toyList){
+            if (item.getToyName().equals(toyName)) return item;
+        }
+        return null;
+    }
+    public void calculateBagAmount(){ //print shopping bag items and calculate price amount
+        double bagAmount = 0;
+        for(Map.Entry<Toy, Integer> entry: shoppingBag.entrySet()){
+            System.out.println(entry.getKey().getToyName() + "- įdėta " + entry.getValue() + " vnt., suma: " + entry.getKey().getPrice() * entry.getValue());
+            bagAmount += entry.getKey().getPrice() * entry.getValue();
+        }
+        System.out.println("Krepšelio prekių suma: " + bagAmount);
+    }
 
 }
